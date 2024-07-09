f5061ff585bd2efae8c91bf3bd60794f83c419ef
Naseem Khan <naseemsaffy@gmail.com>
2024-07-09 11:15:47 +0500

    Client Setup Done

---
+++ client/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui";
+
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#724FF9",
+      },
+    },
+  },
+  plugins: [daisyui],
+};
