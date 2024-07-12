473f4e91b20ab3748595240aed2107e7d930c0c8
NaseemKhan005 <naseemsaffy@gmail.com>
2024-07-12 04:21:38 -0700

    feat: Add emoji picker to conversation footer

--- client/src/components/home/conversationFooter.jsx
+++ client/src/components/home/conversationFooter.jsx
@@ -1,27 +1,43 @@
+import { useState } from "react";
 import { BsEmojiSmile } from "react-icons/bs";
-import { LuPlus } from "react-icons/lu";
+import { GrAttachment } from "react-icons/gr";
 import { PiPaperPlaneRightLight } from "react-icons/pi";
+import Picker from "emoji-picker-react";
 
 const ConversationFooter = () => {
+  const [chosenEmoji, setChosenEmoji] = useState(null);
+  const [showPicker, setShowPicker] = useState(false);
+
+  const onEmojiClick = (event, emojiObject) => {
+    setChosenEmoji(emojiObject);
+    setShowPicker(false);
+  };
+
   return (
-    <div className="w-full bg-white flex items-center gap-3 border-b border-l px-5 py-3">
-      <button>
+    <div className="w-full bg-white flex items-center gap-4 border-b border-l px-5 py-3 relative">
+      <button onClick={() => setShowPicker((val) => !val)}>
         <BsEmojiSmile className="size-6" />
       </button>
+      {showPicker && (
+        <div className="absolute bottom-full mb-2">
+          <Picker onEmojiClick={onEmojiClick} />
+        </div>
+      )}
       <button>
-        <LuPlus className="size-6" />
+        <GrAttachment className="size-5" />
       </button>
 
       <div className="w-full">
         <input
           type="text"
           placeholder="Type here"
-          className="border p-3.5 rounded-lg w-full focous:outline-none focous-within:outline-none  outline-none"
+          value={chosenEmoji ? chosenEmoji.emoji : ""}
+          className="border p-3.5 rounded-lg w-full focus:outline-none focus-within:outline-none outline-none"
         />
       </div>
 
       <button className="p-3.5 px-4 rounded-lg bg-primary text-white hover:bg-indigo-500">
-         <PiPaperPlaneRightLight className="size-6" />
+        <PiPaperPlaneRightLight className="size-6" />
       </button>
     </div>
   );
